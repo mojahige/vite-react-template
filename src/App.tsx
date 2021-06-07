02eaa4a3eb1f82866c6902d918f53cbf1f0d399b
mojahige <mojahige@gmail.com>
2021-06-07 21:08:46 +0900

    remove test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-5xl italic font-bold">Hello Vite + React!</h1>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <main>
+      <h1
+        className="
+          text-5xl
+          italic
+          font-bold
+        "
+      >
+        Hello world
+      </h1>
+    </main>
   );
 }
 
